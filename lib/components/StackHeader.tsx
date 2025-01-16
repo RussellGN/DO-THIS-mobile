8ac5f72ddbbc8309d82a54e1b3997cf944e37a26
Russellg <dudurussell@gmail.com>
2025-01-16 15:45:46 +0200

    feat: use iconSize constant for icon sizes in StackHeader and TabsHeader components

--- lib/components/StackHeader.tsx
+++ lib/components/StackHeader.tsx
@@ -1,19 +1,22 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { View, Text } from "react-native";
 import { capitalize } from "lodash";
-import { darkStyles } from "../constants";
+import { darkStyles, iconSize } from "../constants";
 import { Link } from "expo-router";
 
 export default function StackHeader({ route }: { route: string }) {
-   return (
-      <View style={{ ...darkStyles.container, position: "relative" }} className="pb-3 flex flex-row justify-center items-center">
-         <Link href=".." style={{ position: "absolute", top: 0, left: 0 }}>
-            <MaterialIcons name="arrow-back" size={32} color="gray" />
-         </Link>
+  return (
+    <View
+      style={{ ...darkStyles.container, position: "relative" }}
+      className="pb-3 flex flex-row justify-center items-center"
+    >
+      <Link href=".." style={{ position: "absolute", top: 0, left: 0 }}>
+        <MaterialIcons name="arrow-back" size={iconSize} color="gray" />
+      </Link>
 
-         <Text className="text-white text-center" style={{ fontSize: 24 }}>
-            {capitalize(route)}
-         </Text>
-      </View>
-   );
+      <Text className="text-white text-center" style={{ fontSize: 24 }}>
+        {capitalize(route)}
+      </Text>
+    </View>
+  );
 }
--- lib/components/TabsHeader.tsx
+++ lib/components/TabsHeader.tsx
@@ -2,18 +2,21 @@ import { Link } from "expo-router";
 import { View, Text } from "react-native";
 import { capitalize } from "lodash";
 import { MaterialIcons } from "@expo/vector-icons";
-import { darkStyles, theme } from "../constants";
+import { darkStyles, iconSize, theme } from "../constants";
 
 export default function TabsHeader({ route }: { route: string }) {
-   return (
-      <View style={{ ...darkStyles.container }} className="pb-3 flex flex-row items-center justify-between">
-         <Text className="text-white" style={{ fontSize: 24 }}>
-            {capitalize(route)}
-         </Text>
+  return (
+    <View
+      style={{ ...darkStyles.container }}
+      className="pb-3 flex flex-row items-center justify-between"
+    >
+      <Text className="text-white" style={{ fontSize: 24 }}>
+        {capitalize(route)}
+      </Text>
 
-         <Link href="/about">
-            <MaterialIcons name="info-outline" size={32} color={theme.pallete.primary} />
-         </Link>
-      </View>
-   );
+      <Link href="/about">
+        <MaterialIcons name="info-outline" size={iconSize} color={theme.pallete.primary} />
+      </Link>
+    </View>
+  );
 }
