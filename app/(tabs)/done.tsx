bf21d0b397093324e381cc4edb8ad87e23ac34c7
Russellg <dudurussell@gmail.com>
2025-01-16 16:42:51 +0200

    feat: build Done screen

--- app/(tabs)/done.tsx
+++ app/(tabs)/done.tsx
@@ -1,10 +1,19 @@
 import PageSkeleton from "@/lib/components/PageSkeleton";
-import { Text } from "react-native";
+import Todo from "@/lib/components/Todo";
+import useTodos from "@/lib/hooks/useTodos";
+import { FlatList, Text } from "react-native";
 
 export default function Done() {
-   return (
-      <PageSkeleton>
-         <Text className="text-white">Done</Text>
-      </PageSkeleton>
-   );
+  const { todos } = useTodos();
+
+  return (
+    <PageSkeleton>
+      <FlatList
+        className="py-4 px-2"
+        data={todos}
+        renderItem={({ item }) => <Todo todo={item} />}
+        keyExtractor={(item) => item.id}
+      />
+    </PageSkeleton>
+  );
 }
