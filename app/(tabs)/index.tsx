5a1b4e73116feecde428fa463acdda85007a94e4
Russellg <dudurussell@gmail.com>
2025-01-16 15:47:11 +0200

    feat: integrate TodoSearch and TodoForm components into homescreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,26 @@
 import PageSkeleton from "@/lib/components/PageSkeleton";
-import { Text } from "react-native";
+import Todo from "@/lib/components/Todo";
+import TodoForm from "@/lib/components/TodoForm";
+import TodoSearch from "@/lib/components/TodoSearch";
+import useTodos from "@/lib/hooks/useTodos";
+import { FlatList, ScrollView, View } from "react-native";
 
 export default function Index() {
-   return (
-      <PageSkeleton>
-         <Text className="text-white">Todos</Text>
-      </PageSkeleton>
-   );
+  const { todos } = useTodos();
+
+  return (
+    <PageSkeleton>
+      <TodoSearch />
+
+      <ScrollView showsVerticalScrollIndicator={false} className="py-4 px-2">
+        <FlatList
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} />}
+          keyExtractor={(item) => item.id}
+        />
+      </ScrollView>
+
+      <TodoForm />
+    </PageSkeleton>
+  );
 }
