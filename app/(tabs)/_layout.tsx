0136f1239d926eb2308778276e581d070d6f5941
Russellg <dudurussell@gmail.com>
2025-01-16 12:34:17 +0200

    feat: customize tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,35 @@
-import { Link, Tabs } from "expo-router";
+import TabsHeader from "@/lib/components/TabsHeader";
+import { theme, darkStyles } from "@/lib/constants";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Text } from "react-native";
 
 export default function TabsLayout() {
    return (
       <Tabs
          screenOptions={{
-            headerRight: function () {
-               return <Link href="/about">About</Link>;
+            header: ({ route }) => <TabsHeader route={route.name === "index" ? "Todos" : route.name} />,
+            tabBarStyle: {
+               ...darkStyles.container,
+               height: 60,
             },
+            tabBarLabel: ({ focused, children }) => <Text style={{ color: focused ? theme.pallete.primary : "gray", marginTop: 4 }}>{children}</Text>,
          }}
-      ></Tabs>
+      >
+         <Tabs.Screen
+            name="index"
+            options={{
+               title: "Todos",
+               tabBarIcon: ({ focused }) => <MaterialIcons name="home" color={focused ? theme.pallete.primary : "gray"} size={32} />,
+            }}
+         />
+         <Tabs.Screen
+            name="done"
+            options={{
+               title: "Done",
+               tabBarIcon: ({ focused }) => <MaterialIcons name="done" color={focused ? theme.pallete.primary : "gray"} size={32} />,
+            }}
+         />
+      </Tabs>
    );
 }
