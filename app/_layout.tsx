42d194834ab8db0a43d2c8653a9322eb6dc549d3
Russellg <dudurussell@gmail.com>
2025-01-16 12:31:52 +0200

    feat: update root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
-import { Link, Stack } from "expo-router";
 import "./global.css";
+import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { darkStyles, theme } from "@/lib/constants";
 
 export default function RootLayout() {
    return (
-      <Stack
-         screenOptions={{
-            headerShown: false,
-         }}
-      />
+      <SafeAreaProvider style={{ ...darkStyles.container, padding: 8 }}>
+         <StatusBar barStyle={"light-content"} backgroundColor={theme.pallete.dark[100]} />
+         <Stack
+            screenOptions={{
+               headerShown: false,
+            }}
+         />
+      </SafeAreaProvider>
    );
 }
