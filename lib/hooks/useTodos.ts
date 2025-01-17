404653b95501ce90869ed77a93ada5bbc0494f73
Russellg <dudurussell@gmail.com>
2025-01-17 14:07:29 +0200

    feat: refactor useTodos hook to support async loading and refreshing of todos

--- lib/hooks/useTodos.ts
+++ lib/hooks/useTodos.ts
@@ -1,29 +1,47 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
 import { Todo } from "../types";
+import { Alert } from "react-native";
 
 export default function useTodos() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | undefined>();
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
-  function loadTodos() {
+  async function loadTodosAsync() {
     setIsLoading(true);
     setError(undefined);
 
-    AsyncStorage.getItem("todos")
-      .then((serializedTodos) => {
-        if (serializedTodos) setTodos(JSON.parse(serializedTodos));
-        else setTodos([]);
-        setIsLoading(false);
+    try {
+      const serializedTodos = await AsyncStorage.getItem("todos");
+      if (serializedTodos) setTodos(JSON.parse(serializedTodos));
+      else setTodos([]);
+    } catch (reason) {
+      setError(String(reason));
+    }
+
+    setIsLoading(false);
+  }
+
+  function loadTodos() {
+    loadTodosAsync().finally();
+  }
+
+  function refresh() {
+    setIsRefreshing(true);
+
+    loadTodosAsync()
+      .then(() => {
+        setIsRefreshing(false);
       })
       .catch((reason) => {
-        setError(reason);
-        setIsLoading(false);
+        setError(`Failed to refresh todos: ${reason}`);
+        setIsRefreshing(false);
       });
   }
 
   useEffect(loadTodos, []);
 
-  return { todos, isLoading, error, loadTodos };
+  return { todos, isLoading, error, isRefreshing, refresh, setIsRefreshing, loadTodos };
 }
