14dd2160e62406d342d3ac1f2a75bf90c2ab095e
Russellg <dudurussell@gmail.com>
2025-01-17 14:53:28 +0200

    feat: add filterTodos utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,19 @@
+import { Filter, FilterKeys, Todo } from "./types";
+
 export function generateId() {
   return Math.random().toString(36).slice(2, 9);
+}
+
+export function filterTodos(todos: Todo[], filter: Filter) {
+  return todos.filter((todo) => {
+    let inc = true;
+    const filterBy = (key: FilterKeys) => Object.keys(filter).includes(key);
+
+    if (filterBy("done")) inc = inc && todo.done === filter.done;
+    if (filterBy("query")) inc = inc && todo.content.includes(filter.query!);
+    if (filterBy("startDate")) inc = inc && new Date(todo.date) >= new Date(filter.startDate!);
+    if (filterBy("endDate")) inc = inc && new Date(todo.date) <= new Date(filter.endDate!);
+
+    return inc;
+  });
 }
