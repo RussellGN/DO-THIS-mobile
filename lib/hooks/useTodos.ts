54108f6e1c78f02ed5eaeb3ca601d0dedf467edc
Russellg <dudurussell@gmail.com>
2025-01-17 10:40:21 +0200

    feat: implement useTodos hook with AsyncStorage integration

--- lib/hooks/useTodos.ts
+++ lib/hooks/useTodos.ts
@@ -1,5 +1,27 @@
-import mockTodos from "@/lib/mockData.json";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { Todo } from "../types";
 
 export default function useTodos() {
-  return { todos: mockTodos };
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>();
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(undefined);
+
+    AsyncStorage.getItem("todos")
+      .then((serializedTodos) => {
+        if (serializedTodos) setTodos(JSON.parse(serializedTodos));
+        else setTodos([]);
+        setIsLoading(false);
+      })
+      .catch((reason) => {
+        setError(reason);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return { todos, isLoading, error };
 }
