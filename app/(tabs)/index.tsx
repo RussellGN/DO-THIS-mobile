b92a24ab8e0827f8be65950c15964f73ed37f2b7
Russellg <dudurussell@gmail.com>
2025-01-17 10:40:31 +0200

    feat: enhance todo list with loading and error handling states

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,15 @@ import PageSkeleton from "@/lib/components/PageSkeleton";
 import Todo from "@/lib/components/Todo";
 import TodoForm from "@/lib/components/TodoForm";
 import TodoSearch from "@/lib/components/TodoSearch";
+import { iconSize, theme } from "@/lib/constants";
 import useTodos from "@/lib/hooks/useTodos";
-import { FlatList, ScrollView, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { truncate } from "lodash";
+import { ActivityIndicator, FlatList, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
-  const { todos } = useTodos();
-
+  const { todos, isLoading, error } = useTodos();
+  console.log(error);
   return (
     <PageSkeleton>
       <TodoSearch />
@@ -17,6 +20,19 @@ export default function Index() {
         data={todos}
         renderItem={({ item }) => <Todo todo={item} />}
         keyExtractor={(item) => item.id}
+        ListEmptyComponent={() =>
+          isLoading ? (
+            <ActivityIndicator size="large" color={theme.pallete.primary} />
+          ) : error ? (
+            <Text className="text-[#a9a9a9] text-center p-10">
+              <MaterialIcons name="warning" color={theme.pallete.danger} size={iconSize} />
+              {"\n"}
+              Error encountered fetching todos: {truncate(error, { length: 200 })}
+            </Text>
+          ) : (
+            <Text className="text-[#a9a9a9] text-center p-10">No todos found, lets add some!</Text>
+          )
+        }
       />
 
       <TodoForm />
